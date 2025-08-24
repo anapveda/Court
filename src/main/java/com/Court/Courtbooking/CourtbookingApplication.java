282bbc1e70ba0c277816679be659c42e0f402b9f
vedasree <anapved8@gmail.com>
2025-08-24 15:51:45 +0530

    Court Entity

--- src/main/java/com/Court/Courtbooking/CourtbookingApplication.java
+++ src/main/java/com/Court/Courtbooking/CourtbookingApplication.java
@@ -2,11 +2,12 @@ package com.Court.Courtbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableDiscoveryClient/*enough since we want this to be discovered not calling any other one*/
 public class CourtbookingApplication {
 
 	public static void main(String[] args) {
